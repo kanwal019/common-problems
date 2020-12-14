d643b3ad42e3b8515d6f64270a89d312ee249970
kanwal019 <kanwal019@gmail.com>
2020-12-14 21:06:07 +0530

    Added a program to Reverse a string

---
+++ CSharp/Strings/ReverseString.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Reverses the a string
+ * 
+ * Reads input string from the console
+ *  
+ * Displays output(Reversed string) to the console
+ */
+
+using System;
+
+namespace CSharp.Strings
+{
+    public class ReverseString
+    {
+        public static void DisplayResult()
+        {
+            var str = Console.ReadLine().ToLower();
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                char temp = (i == str.Length - 1) ? Char.ToUpper(str[i]) : str[i];
+                Console.Write(temp);
+            }
+        }
+    }
+}
