d4e51ec029d0e54af469be887566e8e16d9a759d
kanwal019 <kanwal019@gmail.com>
2020-12-27 12:49:25 +0530

    Added a program for bubble sort

---
+++ CSharp/Common/Display.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Common
+{
+    public class Display
+    {
+        public static void Array<T>(T[] array)
+        {
+            int n = array.Length;
+            for (int i = 0; i < n; ++i)
+            {
+                Console.Write(array[i] + " ");
+            }        
+            Console.WriteLine();
+        }
+    }
+}
