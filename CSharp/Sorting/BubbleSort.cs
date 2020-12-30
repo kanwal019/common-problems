1ecd88798f7d316c5a2c6e96fb60f38ab6d7f6aa
kanwal019 <kanwal019@gmail.com>
2020-12-30 22:56:33 +0530

    Added Generic method for Bubble Sort

--- CSharp/Sorting/BubbleSort.cs
+++ CSharp/Sorting/BubbleSort.cs
@@ -6,16 +6,16 @@ namespace CSharp.Sorting
 {
     public class BubbleSort
     {
-        public static void Sort(int[] array)
+        public static void Sort<T>(T[] array) where T : IComparable
         {
             int count = array.Length;
             for (int i = 0; i < count - 1; i++)
             {
                 for (int j = 0; j < count - i - 1; j++)
                 {
-                    if (array[j] > array[j + 1])
+                    if (array[j].CompareTo(array[j + 1]) > 0)
                     {
-                        int temp = array[j];
+                        var temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
                     }
@@ -24,16 +24,16 @@ namespace CSharp.Sorting
             Common.Display.Array(array);
         }
 
-        public static void Descending(int[] array)
+        public static void Descending<T>(T[] array) where T : IComparable
         {
             int count = array.Length;
             for (int i = 0; i < count - 1; i++)
             {
                 for (int j = 0; j < count - i - 1; j++)
                 {
-                    if (array[j] < array[j + 1])
+                    if (array[j].CompareTo(array[j + 1]) < 0)
                     {
-                        int temp = array[j];
+                        var temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
                     }
@@ -42,9 +42,9 @@ namespace CSharp.Sorting
             Common.Display.Array(array);
         }
 
-        public static void Optimised(int[] array)
+        public static void Optimised<T>(T[] array) where T : IComparable
         {
-            int i, j, temp;
+            int i, j;
             int count = array.Length;
             bool swapped;
 
@@ -53,9 +53,9 @@ namespace CSharp.Sorting
                 swapped = false;
                 for (j = 0; j < count - i - 1; j++)
                 {
-                    if (array[j] > array[j + 1])
+                    if (array[j].CompareTo(array[j + 1]) > 0)
                     {
-                        temp = array[j];
+                        var temp = array[j];
                         array[j] = array[j + 1];
                         array[j + 1] = temp;
                         swapped = true;
