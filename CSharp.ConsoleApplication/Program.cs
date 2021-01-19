5ec70b58e10053eebd1ef726d3fb34fbbf16a08a
kanwal019 <kanwal019@gmail.com>
2021-01-19 14:48:21 +0530

    Program to print the min of common elements from two arrays

--- CSharp.ConsoleApplication/Program.cs
+++ CSharp.ConsoleApplication/Program.cs
@@ -11,7 +11,7 @@ namespace CSharp.ConsoleApplication
             //int[] array = { 9, 6, 4, 1, 3, 7, 8, 5, 0, 2 };
             //CSharp.Sorting.BubbleSort.Descending(array);
             
-            CSharp.Arrays.PrintUncommonElements.DisplayResult();
+            CSharp.Arrays.PrintCommonElements.DisplayResult();
         }       
     }
 }
