8bf9bea4d4d9763e46bb3e52686e9ab011832495
kanwal019 <kanwal019@gmail.com>
2021-02-10 15:35:44 +0530

    Added a program to find product of sum

---
+++ CSharp/Other/ProductOfSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Other
+{
+    public class ProductOfSum
+    {
+        public static void DisplayResult()
+        {
+            DisplayPOS(1);
+            DisplayPOS(2);
+            DisplayPOS(3);
+            DisplayPOS(4);
+        }
+
+        static void DisplayPOS(int num)
+        {
+            int prod = 1;
+            while (num > 0)
+            {
+                // prod *= (num + (num + 1) + (num + 2));
+                prod *= (3 * (num + 1));
+                num -= 1;
+            }
+            Console.WriteLine(prod);
+        }
+    }
+}
