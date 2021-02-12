d90a0100868ac4c756efda0d87619db5ea6f6e71
kanwal019 <kanwal019@gmail.com>
2021-02-12 09:49:23 +0530

    Program to find a pair with the sum closest to the given number

---
+++ CSharp/Arrays/PairAndClosestSum.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * In a given array find a pair whose sum matches the mentioned integer
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp.Arrays
+{
+    public class PairAndClosestSum
+    {
+        public static void DisplayResult()
+        {
+            int[] input = { 1, 5, 22, 10, 17 };
+            int sum = 12;
+
+            GetClosestPair(input, sum);
+        }
+
+        private static void GetClosestPair(int[] input, int sum)
+        {
+            Dictionary<int, string> diffPairs = new Dictionary<int, string>();
+            int nearest = sum;
+            string temp = String.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input.Length; j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+
+                    int key = Math.Abs(sum - (input[j] + input[i]));
+
+                    if (!diffPairs.TryGetValue(key, out temp))
+                        diffPairs.Add((key), input[j] + "," + input[i]);
+                }
+            }
+
+            foreach (var pair in diffPairs)
+            {
+                if (pair.Key < nearest)
+                {
+                    nearest = pair.Key;
+                }
+            }
+
+            Console.WriteLine("(" + diffPairs[nearest] + ")");
+        }
+    }
+}
