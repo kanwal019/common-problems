4b7028dcfc264b0b919a7cc122f98ada99da7444
kanwal019 <kanwal019@gmail.com>
2023-02-22 00:39:57 +0530

    ✨Added a program to find the second to the largest missing number from an array

--- CSharp.ConsoleApplication/Program.cs
+++ CSharp.ConsoleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace CSharp.ConsoleApplication
         static void Main(string[] args)
         {
             int[] array = { 9, 6, 4, 1, 3, 7, 8, 5, 0, 2 };
-            Searching.SmallestMissingNumber.Find(array);
+            Searching.SecondLargestMissingNumber.Find(array);
 
             //Strings.ReverseWords.DisplayResult();                
         }        
