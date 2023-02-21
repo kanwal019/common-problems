7f7a4983917fd0dab1db19388306419351f34b27
kanwal019 <kanwal019@gmail.com>
2023-02-22 00:39:57 +0530

    ✨Added a program to find the second to the largest missing number from an array

---
+++ CSharp/Searching/SecondLargestMissingNumber.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Write a program to find the second to the largest missing number(N) from an array. N >= 1
+ */
+
+using System;
+
+namespace CSharp.Searching
+{
+    public class SecondLargestMissingNumber
+    {
+        public static void Find(int[] arr)
+        {
+            Array.Sort(arr);
+            Array.Reverse(arr);
+
+            int largestMissing = arr[arr.Length - 1];
+            largestMissing = largestMissing >= 1 ? largestMissing : 1;
+
+            foreach (int element in arr)
+            {
+                if (element > 0)
+                {
+                    if (largestMissing == element)
+                    {
+                        largestMissing -= 1;
+                    }
+                    else if (largestMissing > element)
+                    {
+                        Console.WriteLine(largestMissing);
+                        return;
+                    }
+                }
+            }
+            Console.WriteLine(largestMissing);
+        } 
+    }
+}
