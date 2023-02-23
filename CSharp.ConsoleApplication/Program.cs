4fbdb930bda04fba5f21ceb81fb3c9a86a642900
kanwal019 <kanwal019@gmail.com>
2023-02-23 20:05:20 +0530

    ✨Added a program to manage the seats for a familiy in an airplane

--- CSharp.ConsoleApplication/Program.cs
+++ CSharp.ConsoleApplication/Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CSharp.ConsoleApplication
+﻿namespace CSharp.ConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array = { 9, 6, 4, 1, 3, 7, 8, 5, 0, 2 };
-            Searching.SecondLargestMissingNumber.Find(array);
-
-            //Strings.ReverseWords.DisplayResult();                
-        }        
+            int num = 2;
+            string str = "1A 2F 1C";
+            Strings.SeatManagement.DisplayResult(num, str);
+        }
     }
 }
